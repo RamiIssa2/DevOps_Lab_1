b340ccb8aa7d175ed63d7b98381efb37693124c0
Rami Issa <rami.issa.ncd@gmail.com>
2025-04-10 13:19:04 +0300

    database controller

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from init_db import create_db
+from db_controller import run_app
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     create_db()
+    print("Database initialized.")
+    run_app()
