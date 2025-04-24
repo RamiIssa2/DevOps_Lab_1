aa5fcc694fcf2be0d9d3027a30d19ee8b75fb66a
Rami Issa <rami.issa.ncd@gmail.com>
2025-04-24 13:08:01 +0300

    Add App.jsx

---
+++ task-manager-frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
+
+function App() {
+  return (
+    <div>
+      <h1>Task Manager</h1>
+      <TaskForm onTaskCreated={() => window.location.reload()} />
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
