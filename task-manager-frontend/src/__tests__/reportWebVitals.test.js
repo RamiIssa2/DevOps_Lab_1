6cca7087c728fb6ea1c54b1f597cf33b66a5ea90
Rami Issa <rami.issa.ncd@gmail.com>
2025-09-18 19:03:34 +0300

    Add additional frontend tests

--- task-manager-frontend/src/__tests__/reportWebVitals.test.js
+++
@@ -1,7 +0,0 @@
-import reportWebVitals from '../reportWebVitals';
-
-test('reportWebVitals calls callback if provided', () => {
-  const mockFn = jest.fn();
-  reportWebVitals(mockFn);
-  expect(mockFn).not.toHaveBeenCalled(); // still covers the branch where cb exists
-});
